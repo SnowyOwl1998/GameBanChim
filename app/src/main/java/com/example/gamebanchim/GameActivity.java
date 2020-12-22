4223ee47140a32561f12f8a9cc052f976dd42370
quan <vutnhan03@gmail.com>
2020-12-22 22:25:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gamebanchim/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.gamebanchim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
